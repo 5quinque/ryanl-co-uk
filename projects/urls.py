54f2a94d778f97f76f6d1418a6f60580b6449b96
Ryan Linnit <ryan@ryanl.co.uk>
2021-08-25 08:16:20 +0100

    project has its own page

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, ProjectView
 
-urlpatterns = [path("", IndexView.as_view(), name="index")]
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("project/<str:project_name>/", ProjectView.as_view(), name="View"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -10,3 +11,14 @@ class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         projects = Project.objects.order_by("-pub_date")
         return render(request, self.template_name, {"projects": projects})
+
+
+class ProjectView(TemplateView):
+    template_name = "project.html"
+
+    def get(self, request, *args, **kwargs):
+        try:
+            project = Project.objects.get(name_text=kwargs.get("project_name"))
+        except Project.DoesNotExist:
+            raise Http404("Project does not exist")
+        return render(request, self.template_name, {"project": project})
